5e08da5e777e4913ed458feb804ee418bd41917a
jambonbill <jambonbill@gmail.com>
2015-02-13 11:15:37 +0800

    moved

--- student_constancy.php
+++
@@ -1,8 +0,0 @@
-<?php
-// student constancy
-?>
-<!-- AdminLte theme (for xs progressbars) -->
-<h3><i class='fa fa-ellipsis-h'></i> Constancy <small id='moreConstant'></small></h3>
-<div id='constantDiv'></div>
-
-<script src='js/student_constancy.js'></script>
